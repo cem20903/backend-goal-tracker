712b4a004b081cf75557a99f7cea1e2d7672252e
cem20903 <cem20903@gmail.com>
2024-03-18 10:07:39 +0100

    Add HS diary

--- routes/HS.js
+++ routes/HS.js
@@ -1,201 +1,347 @@
-
 import express from "express";
-import collection from '../server.js'
+import collection from "../server.js";
 import { calculateRead } from "../utils/calculateBooks.js";
-import { uid } from 'uid';
+import { uid } from "uid";
 const app = express();
 
+app.get("/hs-books-records", async (req, res) => {
+	let user = await collection.findOne({ email: "cem20903@gmail.com" });
 
-app.get('/hs-books-records', async (req, res) => {
+	const { HSBooks, HSBooksToRead } = user;
 
-  let user = await collection.findOne({ email: 'cem20903@gmail.com' });
-    
-  const { HSBooks, HSBooksToRead } = user
-  
-  const buildCorrectResponse = HSBooksToRead.map(book => {
-  
-    const booksById = HSBooks.filter(recordBook => recordBook.id === book.id)
+	const buildCorrectResponse = HSBooksToRead.map((book) => {
+		const booksById = HSBooks.filter((recordBook) => recordBook.id === book.id);
 
-    if(booksById.length > 0) {
-      const bestRecordBook = booksById.sort((bookA, bookB) => bookB.current - bookA.current)[0]
-      
-      return bestRecordBook
-    }
-   
-    return book
-  })
-  
-  const totalPages = HSBooks.map(book => book.total).reduce((acc, pages) => acc + pages,0)
-  
-  const totalCurrent = HSBooks.map(book => book.current).reduce((acc, pages) => acc + pages,0)
-    
-  const average = Math.round((totalCurrent * 100 / totalPages) * 100) / 100
-  
-  res.json({ books: buildCorrectResponse, average })
-})
+		if (booksById.length > 0) {
+			const bestRecordBook = booksById.sort(
+				(bookA, bookB) => bookB.current - bookA.current
+			)[0];
 
+			return bestRecordBook;
+		}
 
-const recordOnFutureBackend = [{
-  id: 'DAYS_WORKED',
-  date: new Date(),
-  record: 0
-}]
+		return book;
+	});
 
-app.get('/hs-records', async (req, res) => {
+	const totalPages = HSBooks.map((book) => book.total).reduce(
+		(acc, pages) => acc + pages,
+		0
+	);
 
+	const totalCurrent = HSBooks.map((book) => book.current).reduce(
+		(acc, pages) => acc + pages,
+		0
+	);
 
-let { HSRecords, HSRecordsTracking } = await collection.findOne({ email: 'cem20903@gmail.com' });
+	const average = Math.round(((totalCurrent * 100) / totalPages) * 100) / 100;
 
-const recordsWithUpdates = HSRecords.map(record => {
-
-  const totalRecords = HSRecordsTracking.filter(trackRecord => trackRecord.id === record.id)
-  
-  const sumTotal = totalRecords.reduce((acc, record) => acc + record.record, 0)
-  
-  const percentaje = Math.round((sumTotal * 100 / record.record) * 100) / 100
-
-  return {
-    ...record,
-    record: sumTotal,
-    percentaje,
-    total: record.record
-  }
-})
-
-const average = Math.round((recordsWithUpdates.reduce((acc, record) => acc + record.percentaje, 0) / recordsWithUpdates.length) * 100) / 100
-
-res.json({ records: recordsWithUpdates, avg: average })
-
-})
-
-app.get('/hs-books', async (req, res) => {
-
-  const { date } = req.query
-    
-  let user = await collection.findOne({ email: 'cem20903@gmail.com' });
-  
-  const { HSBooks, HSBooksToRead } = user
-  
-  const booksWithDate = HSBooks.filter(book => {
-    const booksDate = new Date(book.date)
-    booksDate.setHours(0,0,0,0)
-    
-    const dateFiltered = new Date(date)
-    dateFiltered.setHours(0,0,0,0)
-   return booksDate.getTime() === dateFiltered.getTime()
-  })
-    
-  const buildCorrectResponse = HSBooksToRead.map(book => {
-    const bookFounded = booksWithDate.find(booksWithDate => booksWithDate.id === book.id)
-  
-    if(bookFounded) {
-      return bookFounded
-    }
-    
-    return book
-  })
-  
-  
-  res.json({ books: buildCorrectResponse })
-})
-
-
-app.post('/hs-new-book', async (req, res) => {
- 
-  const { title, total, date } = req.body
-  
-   let user = await collection.findOne({ email: 'cem20903@gmail.com' });
-   
-   const { HSBooksToRead } = user
-   
-   const newBook = { title, total: parseFloat(total), current: 0, date: new Date(date), id: uid() }
-   
-   const copyBooksToRead = [...HSBooksToRead]
-   
-   copyBooksToRead.push(newBook)
-   
-   await collection.replaceOne({ email: 'cem20903@gmail.com' }, {...user, HSBooksToRead: copyBooksToRead })
-   
-   res.json(newBook)
- })
- 
- app.post('/add-hs-books-updated', async (req, res) => {
-  
-  const { booksUpdated } = req.body
-  
-  let user = await collection.findOne({ email: 'cem20903@gmail.com' });
-  
-  const { HSBooks } = user
-  
-  const booksUpdatedRecords = [...HSBooks, ...booksUpdated]
-  
-  await collection.replaceOne({ email: 'cem20903@gmail.com' }, {...user, HSBooks: booksUpdatedRecords })
-  
-  res.json({});
+	res.json({ books: buildCorrectResponse, average });
 });
 
-app.post('/set-hs-records', async (req, res) => {
+const recordOnFutureBackend = [
+	{
+		id: "DAYS_WORKED",
+		date: new Date(),
+		record: 0,
+	},
+];
 
-  const user = await collection.findOne({ email: 'cem20903@gmail.com' });
-  
-  let { HSRecordsTracking, HSRecords } = user
+app.get("/hs-records", async (req, res) => {
+	let { HSRecords, HSRecordsTracking } = await collection.findOne({
+		email: "cem20903@gmail.com",
+	});
 
-  let { currentRecords } = req.body
-  
-  currentRecords = currentRecords.map(currentRecord => {
-    return {
-      ...currentRecord,
-      date: new Date(currentRecord.date),
-      record: currentRecord.record
-    }
-    
-    
-  
-  })
+	const recordsWithUpdates = HSRecords.map((record) => {
+		const totalRecords = HSRecordsTracking.filter(
+			(trackRecord) => trackRecord.id === record.id
+		);
 
-   currentRecords = HSRecords.map(baseRecord => {
-   
-   const exist = currentRecords.find(currentRecord => currentRecord.title === baseRecord.title)
-   
-   if(exist) {
-    return {...exist} 
-  }
-  
-  return {...baseRecord}
-   
-   
-   })
-     
-  
-  let currentHSRecords = [...HSRecordsTracking]
-  
-  let existNewRecord = false
-  
-  const searchAndReplace = currentHSRecords.map(current => {
-  
-    const newRecordFounded = currentRecords.find(newRecord => current.title === newRecord.title && current.date === newRecord.date)
-    
-    if(newRecordFounded) {
-      existNewRecord = true
-      return newRecordFounded
-    }
-    
-    return current
-  })
+		const sumTotal = totalRecords.reduce(
+			(acc, record) => acc + record.record,
+			0
+		);
 
-  if(!existNewRecord) {
-    currentRecords.forEach(newRecord => {
-      searchAndReplace.push(newRecord)
-    })
-  }
-  
-    
-  
-  await collection.replaceOne({ email: 'cem20903@gmail.com' }, {...user, HSRecordsTracking: searchAndReplace })
-  
-  res.json({})
+		const percentaje =
+			Math.round(((sumTotal * 100) / record.record) * 100) / 100;
 
-})
+		return {
+			...record,
+			record: sumTotal,
+			percentaje,
+			total: record.record,
+		};
+	});
 
+	const average =
+		Math.round(
+			(recordsWithUpdates.reduce((acc, record) => acc + record.percentaje, 0) /
+				recordsWithUpdates.length) *
+				100
+		) / 100;
 
-export default app
+	res.json({ records: recordsWithUpdates, avg: average });
+});
+
+app.get("/hs-books", async (req, res) => {
+	const { date } = req.query;
+
+	let user = await collection.findOne({ email: "cem20903@gmail.com" });
+
+	const { HSBooks, HSBooksToRead } = user;
+
+	const booksWithDate = HSBooks.filter((book) => {
+		const booksDate = new Date(book.date);
+		booksDate.setHours(0, 0, 0, 0);
+
+		const dateFiltered = new Date(date);
+		dateFiltered.setHours(0, 0, 0, 0);
+		return booksDate.getTime() === dateFiltered.getTime();
+	});
+
+	const buildCorrectResponse = HSBooksToRead.map((book) => {
+		const bookFounded = booksWithDate.find(
+			(booksWithDate) => booksWithDate.id === book.id
+		);
+
+		if (bookFounded) {
+			return bookFounded;
+		}
+
+		return book;
+	});
+
+	res.json({ books: buildCorrectResponse });
+});
+
+app.post("/hs-new-book", async (req, res) => {
+	const { title, total, date } = req.body;
+
+	let user = await collection.findOne({ email: "cem20903@gmail.com" });
+
+	const { HSBooksToRead } = user;
+
+	const newBook = {
+		title,
+		total: parseFloat(total),
+		current: 0,
+		date: new Date(date),
+		id: uid(),
+	};
+
+	const copyBooksToRead = [...HSBooksToRead];
+
+	copyBooksToRead.push(newBook);
+
+	await collection.replaceOne(
+		{ email: "cem20903@gmail.com" },
+		{ ...user, HSBooksToRead: copyBooksToRead }
+	);
+
+	res.json(newBook);
+});
+
+app.post("/add-hs-books-updated", async (req, res) => {
+	const { booksUpdated } = req.body;
+
+	let user = await collection.findOne({ email: "cem20903@gmail.com" });
+
+	const { HSBooks } = user;
+
+	const booksUpdatedRecords = [...HSBooks, ...booksUpdated];
+
+	await collection.replaceOne(
+		{ email: "cem20903@gmail.com" },
+		{ ...user, HSBooks: booksUpdatedRecords }
+	);
+
+	res.json({});
+});
+
+app.post("/set-hs-records", async (req, res) => {
+	const user = await collection.findOne({ email: "cem20903@gmail.com" });
+
+	let { HSRecordsTracking, HSRecords } = user;
+
+	let { currentRecords } = req.body;
+
+	currentRecords = currentRecords.map((currentRecord) => {
+		return {
+			...currentRecord,
+			date: new Date(currentRecord.date),
+			record: currentRecord.record,
+		};
+	});
+
+	currentRecords = HSRecords.map((baseRecord) => {
+		const exist = currentRecords.find(
+			(currentRecord) => currentRecord.title === baseRecord.title
+		);
+
+		if (exist) {
+			return { ...exist };
+		}
+
+		return { ...baseRecord };
+	});
+
+	let currentHSRecords = [...HSRecordsTracking];
+
+	let existNewRecord = false;
+
+	const searchAndReplace = currentHSRecords.map((current) => {
+		const newRecordFounded = currentRecords.find(
+			(newRecord) =>
+				current.title === newRecord.title && current.date === newRecord.date
+		);
+
+		if (newRecordFounded) {
+			existNewRecord = true;
+			return newRecordFounded;
+		}
+
+		return current;
+	});
+
+	if (!existNewRecord) {
+		currentRecords.forEach((newRecord) => {
+			searchAndReplace.push(newRecord);
+		});
+	}
+
+	await collection.replaceOne(
+		{ email: "cem20903@gmail.com" },
+		{ ...user, HSRecordsTracking: searchAndReplace }
+	);
+
+	res.json({});
+});
+
+app.get("/hs-diary-tasks", (req, res) => {
+	const diaryTasks = [
+		{
+			title: "Cualificar",
+			completed: false,
+			primary: true,
+			id: "EXERCISE_QUALIFY",
+		},
+		{
+			title: "Sexualizar",
+			completed: false,
+			primary: true,
+			id: "EXERCISE_QUALIFY",
+		},
+		{
+			title: "Ejemplo Cualificar",
+			completed: false,
+			primary: true,
+			id: "EXAMPLE_QUALIFY",
+		},
+		{
+			title: "Ejemplo Sexualizar",
+			completed: false,
+			primary: true,
+			id: "EXAMPLE_SEX",
+		},
+	];
+
+	const currentDate = new Date().getDay();
+
+	const isDayToGame = currentDate === 5 || currentDate === 6;
+
+	if (isDayToGame) {
+		diaryTasks.push({
+			title: "Salida",
+			completed: false,
+			primary: true,
+			id: "GO_GAME",
+		});
+	}
+
+	res.json({ diaryTasks });
+});
+
+app.post("/add-hs-tasks", async (req, res) => {
+	console.log("/add-hs-tasks");
+
+	let { tasks } = req.body;
+
+	tasks = tasks.map((task) => {
+		const date = new Date(task.date);
+		date.setHours(0, 0, 0, 0);
+
+		return {
+			...task,
+			date,
+		};
+	});
+
+	let usuario = await collection.findOne({ email: "cem20903@gmail.com" });
+
+	const { hsDiaryTasks } = usuario;
+
+	if (hsDiaryTasks.length === 0) {
+		await collection.replaceOne(
+			{ email: "cem20903@gmail.com" },
+			{ ...usuario, hsDiaryTasks: tasks }
+		);
+		return res.json({});
+	}
+
+	let existRecord = hsDiaryTasks.filter((task) => {
+		return new Date(task.date).getTime() === new Date(tasks[0].date).getTime();
+	});
+
+	if (!existRecord) {
+		const updatedTasks = [...hsDiaryTasks, ...tasks];
+		await collection.replaceOne(
+			{ email: "cem20903@gmail.com" },
+			{ ...usuario, hsDiaryTasks: updatedTasks }
+		);
+		return res.json({});
+	}
+
+	let someChanged = false;
+
+	let updateTasks = hsDiaryTasks.map((task) => {
+		const exist = tasks.find(
+			(newTask) =>
+				newTask.title === task.title &&
+				new Date(newTask.date).getTime() === new Date(task.date).getTime()
+		);
+		if (exist) {
+			someChanged = true;
+			return exist;
+		}
+		return task;
+	});
+
+	if (!someChanged) {
+		tasks.map((task) => {
+			updateTasks.push(task);
+		});
+	}
+	await collection.replaceOne(
+		{ email: "cem20903@gmail.com" },
+		{ ...usuario, hsDiaryTasks: updateTasks }
+	);
+	return res.json({});
+});
+
+// /get-all-hs-diary
+
+app.get("/get-all-hs-diary", async (req, res) => {
+	console.log("get all HS diary");
+	let user = await collection.findOne({ email: "cem20903@gmail.com" });
+
+	const tasksOrderedByDate = [...user.hsDiaryTasks].sort(
+		(taskA, taskB) => new Date(taskA.date) - new Date(taskB.date)
+	);
+
+	const tasksFilterByCurrentMonth = tasksOrderedByDate.filter(
+		(task) => new Date(task.date).getMonth() === new Date().getMonth()
+	);
+
+	res.json({ tasks: tasksFilterByCurrentMonth });
+});
+
+export default app;
--- routes/summary.js
+++ routes/summary.js
@@ -1,211 +1,339 @@
-
 import express from "express";
-import collection from '../server.js'
+import collection from "../server.js";
 import { calculateRead } from "../utils/calculateBooks.js";
-import { calculateOthers, calculateTotal, calculateEnglish, getWeekEnglishNumbers, getTasksNumbers, buildEnglishComparative, getBuildTasksComparative  } from "../utils/calculateSummary.js";
-import { getWeekNumber } from '../utils/dates.js'
+import {
+	calculateOthers,
+	calculateTotal,
+	calculateEnglish,
+	getWeekEnglishNumbers,
+	getTasksNumbers,
+} from "../utils/calculateSummary.js";
+import { getWeekNumber } from "../utils/dates.js";
 import comparativeWeeks from "../controllers/comparativeWeeks.js";
-import comparativeWeeeksAllTime from '../controllers/comparativeWeeeksAllTime.js'
-import fetch from "node-fetch";
-import { getInfoFitnessWorkout, getRecords } from "../services/fitnessWorkout.js";
+import comparativeWeeeksAllTime from "../controllers/comparativeWeeeksAllTime.js";
+import {
+	getInfoFitnessWorkout,
+	getRecords,
+} from "../services/fitnessWorkout.js";
 
 const app = express();
 
-const BACK_CI_URL = 'https://fitnessworkout.onrender.com'
+function calculateStrikesHS(HSRecords, HSRecordsTracking) {
+	const recordsWithUpdates = HSRecords.map((record) => {
+		const totalRecords = HSRecordsTracking.filter(
+			(trackRecord) => trackRecord.id === record.id
+		);
 
-// const BACK_CI_URL = 'http://localhost:4000'
+		const sumTotal = totalRecords.reduce(
+			(acc, record) => acc + record.record,
+			0
+		);
 
-function calculateStrikesHS (HSRecords, HSRecordsTracking) {
+		const percentaje =
+			Math.round(((sumTotal * 100) / record.record) * 100) / 100;
 
+		return {
+			...record,
+			record: sumTotal,
+			percentaje,
+			total: record.record,
+		};
+	});
 
-  const recordsWithUpdates = HSRecords.map(record => {
+	const average =
+		Math.round(
+			(recordsWithUpdates.reduce((acc, record) => acc + record.percentaje, 0) /
+				recordsWithUpdates.length) *
+				100
+		) / 100;
 
-    const totalRecords = HSRecordsTracking.filter(trackRecord => trackRecord.id === record.id)
-    
-    const sumTotal = totalRecords.reduce((acc, record) => acc + record.record, 0)
-    
-    const percentaje = Math.round((sumTotal * 100 / record.record) * 100) / 100
-  
-    return {
-      ...record,
-      record: sumTotal,
-      percentaje,
-      total: record.record
-    }
-  })
-  
-  const average = Math.round((recordsWithUpdates.reduce((acc, record) => acc + record.percentaje, 0) / recordsWithUpdates.length) * 100) / 100
-  
-  return average
-
+	return average;
 }
 
-function calculateHSBooks (HSBooks) {
-  
-  const totalPages = HSBooks.map(book => book.total).reduce((acc, pages) => acc + pages,0)
-  
-  const totalCurrent = HSBooks.map(book => book.current).reduce((acc, pages) => acc + pages,0)
-    
-  const average = Math.round((totalCurrent * 100 / totalPages) * 100) / 100
-  
-  return average
+function calculateHSBooks(HSBooks) {
+	const totalPages = HSBooks.map((book) => book.total).reduce(
+		(acc, pages) => acc + pages,
+		0
+	);
 
+	const totalCurrent = HSBooks.map((book) => book.current).reduce(
+		(acc, pages) => acc + pages,
+		0
+	);
+
+	const average = Math.round(((totalCurrent * 100) / totalPages) * 100) / 100;
+
+	return average;
 }
 
+app.get("/info-weight", async (req, res) => {
+	const { infoWeight } = await getInfoFitnessWorkout();
+	res.json({ infoWeight });
+});
 
+app.get("/summary", async (req, res) => {
+	const user = await collection.findOne({ email: "cem20903@gmail.com" });
 
-app.get('/summary', async (req, res) => {
-  
-  const user = await collection.findOne({ email: 'cem20903@gmail.com' });
-  
-  const { otherGoals, books, englishRecords, booksToRead, HSRecords, HSRecordsTracking, HSBooks } = user
-  
-  const readPercantage = { title: 'Lectura', percentage: calculateRead(books, booksToRead) }
-  const englishPercentage = { title: 'Ingles', percentage: calculateEnglish(englishRecords) }
-  const appCIPercentage = { title: 'App Coeficiente Iron', percentage: calculateOthers(otherGoals, 'APP_CI') } 
-  const goalTrackerPercentage = { title: 'App Goal Tracker', percentage: calculateOthers(otherGoals, 'GOAL_TRACKER') }
-  
-  const { percentajeWeight, percentajeCI } = await getInfoFitnessWorkout()
-  
-    
-  const CI = { title: 'Coeficiente Iron', percentage: percentajeCI, primary: true }
-  const weight = { title: 'Peso', percentage: percentajeWeight, primary: true }
-  const economy = { title: 'Economia (beta) ', percentage: 51 }
-  
-  
-  // Son ToDo
-  const personalBrand = { title: 'Marca Personal', percentage: calculateOthers(otherGoals, 'PRO_WORK') }
-  const work = { title: 'Carrera Laboral', percentage: calculateOthers(otherGoals, 'WORK') }
-  const instaAlfara = { title: 'Instagram Alfara', percentage: calculateOthers(otherGoals, 'INSTAALFARA') }
-  const first = { title: 'First', percentage: calculateOthers(otherGoals, 'FIRST') }
-  const sport = { title: 'Deporte', percentage: calculateOthers(otherGoals, 'SPORTS') }
-  const house = { title: 'Cosas de Casa', percentage: calculateOthers(otherGoals, 'HOUSE') }
-  
+	const {
+		otherGoals,
+		books,
+		englishRecords,
+		booksToRead,
+		HSRecords,
+		HSRecordsTracking,
+		HSBooks,
+	} = user;
 
-  const hsTotal = [calculateOthers(otherGoals, 'HS'), calculateOthers(otherGoals, 'HS_OTHERS'), calculateStrikesHS(HSRecords, HSRecordsTracking), calculateHSBooks(HSBooks)]
-  
-  const hsAverage = Math.round((hsTotal.reduce((acc, avg) => acc + avg, 0) / hsTotal.length) * 100) / 100
-  
-  const socialSkills = { title: 'Habilidades Sociales', percentage: hsAverage, primary: true }
-  
+	const readPercantage = {
+		title: "Lectura",
+		percentage: calculateRead(books, booksToRead),
+		name: "READ",
+	};
+	const englishPercentage = {
+		title: "Ingles",
+		percentage: calculateEnglish(englishRecords),
+		name: "ENGLISH",
+	};
+	const appCIPercentage = {
+		title: "App Coeficiente Iron",
+		percentage: calculateOthers(otherGoals, "APP_CI"),
+		name: "APP_CI",
+	};
+	const goalTrackerPercentage = {
+		title: "App Goal Tracker",
+		percentage: calculateOthers(otherGoals, "GOAL_TRACKER"),
+		name: "GOAL_TRACKER",
+	};
 
+	const { percentajeWeight, percentajeCI } = await getInfoFitnessWorkout();
 
-  const results = [socialSkills, CI, weight, economy, work, personalBrand, first, house, sport, readPercantage, instaAlfara, englishPercentage, appCIPercentage, goalTrackerPercentage]
-  
-  const total = calculateTotal(results)
+	const CI = {
+		title: "Coeficiente Iron",
+		percentage: percentajeCI,
+		primary: true,
+		name: "CI",
+	};
+	const weight = {
+		title: "Peso",
+		percentage: percentajeWeight,
+		primary: true,
+		name: "WEIGHT",
+	};
 
- 
-  res.json({ results, total })
-})
+	const calculateEconomy = () => {
+		const payCurrent = (new Date().getMonth() + 1) * 188;
+		const saveMoney = 100;
+		const debt = (payCurrent * 100) / 16934;
+		return Math.round(((saveMoney + debt) / 2) * 100) / 100;
+	};
 
+	const economy = {
+		title: "Economia (beta) ",
+		percentage: calculateEconomy(),
+		name: "ECONOMY",
+	};
 
-app.post('/save-summary', async (req, res) => {
+	// Son ToDo
+	const personalBrand = {
+		title: "Marca Personal",
+		percentage: calculateOthers(otherGoals, "PRO_WORK"),
+		name: "PERSONAL_BRAND",
+	};
+	const work = {
+		title: "Carrera Laboral",
+		percentage: calculateOthers(otherGoals, "WORK"),
+		name: "WORK",
+	};
+	const instaAlfara = {
+		title: "Instagram Alfara",
+		percentage: calculateOthers(otherGoals, "INSTAALFARA"),
+		name: "INSTAGRAM_ALFARA",
+	};
+	const first = {
+		title: "First",
+		percentage: calculateOthers(otherGoals, "FIRST"),
+		name: "FIRST",
+	};
+	const sport = {
+		title: "Deporte",
+		percentage: calculateOthers(otherGoals, "SPORTS"),
+		name: "SPORTS",
+	};
+	const house = {
+		title: "Cosas de Casa",
+		percentage: calculateOthers(otherGoals, "HOUSE"),
+		name: "HOUSE",
+	};
 
-  const { currentSummary } = req.body
+	const hsTotal = [
+		calculateOthers(otherGoals, "HS"),
+		calculateOthers(otherGoals, "HS_OTHERS"),
+		calculateStrikesHS(HSRecords, HSRecordsTracking),
+		calculateHSBooks(HSBooks),
+	];
 
-  const user = await collection.findOne({ email: 'cem20903@gmail.com' });
-  
-  const { summary } = user
-  
-  const average = currentSummary.reduce((acc, record) => acc + record.percentage , 0) / currentSummary.length
-  
-  summary.push({ summary: currentSummary, date: new Date(), average })
-  
-  
-  await collection.replaceOne({ email: 'cem20903@gmail.com' }, {...user, summary })
-  
-  res.json({})
+	const hsAverage =
+		Math.round(
+			(hsTotal.reduce((acc, avg) => acc + avg, 0) / hsTotal.length) * 100
+		) / 100;
 
-})
+	const socialSkills = {
+		title: "Habilidades Sociales",
+		percentage: hsAverage,
+		primary: true,
+		name: "HS",
+	};
 
-app.get('/summary-by', async (_, res) => {
+	const results = [
+		socialSkills,
+		CI,
+		weight,
+		economy,
+		work,
+		personalBrand,
+		first,
+		house,
+		sport,
+		readPercantage,
+		instaAlfara,
+		englishPercentage,
+		appCIPercentage,
+		goalTrackerPercentage,
+	];
 
-  const user = await collection.findOne({ email: 'cem20903@gmail.com' });
-  
-  const { otherGoals, englishRecords, diaryTasks } = user
-  
-  const currentWeek = getWeekNumber(new Date())
-  const tasksByWeek = getTasksNumbers(otherGoals)
-  
-  
-  // Diary Task
-  const diaryTasksWithWeekNumber = diaryTasks.map(task => {
-    return {
-      ...task,
-      week: getWeekNumber(task.date)
-    }
-  })
-  
-  const diaryTasksByWeek = diaryTasksWithWeekNumber.filter(task => task.week === currentWeek)
-  
-  const englishRecordsJoined = getWeekEnglishNumbers(englishRecords)
-  
-  res.json({ diaryTasksByWeek, englishRecordsByWeek: englishRecordsJoined, tasksByWeek })
+	const total = calculateTotal(results);
 
-})
+	res.json({ results, total });
+});
 
-app.get('/comparative-weeks', comparativeWeeks)
+app.post("/save-summary", async (req, res) => {
+	const { currentSummary } = req.body;
 
-app.get('/comparative-percentages', async (req, res) => {
+	const user = await collection.findOne({ email: "cem20903@gmail.com" });
 
-  const user = await collection.findOne({ email: 'cem20903@gmail.com' });
-  const { summary } = user
-  
-  res.json(summary)
-})
+	const { summary } = user;
 
-app.get('/comparative-weeks-all-time', comparativeWeeeksAllTime)
+	const average =
+		currentSummary.reduce((acc, record) => acc + record.percentage, 0) /
+		currentSummary.length;
 
+	summary.push({ summary: currentSummary, date: new Date(), average });
 
-app.get('/info-ci', async (req, res) => {
+	await collection.replaceOne(
+		{ email: "cem20903@gmail.com" },
+		{ ...user, summary }
+	);
 
-  const infoCI = await getRecords()
-  
-  const { strongRecords, aerobicRecords, powerRecords, totalCI } = infoCI
-    
-  const names = {
-    PRESS_BANCA: 'Press banca', 
-    PRESS_MILITAR: 'Press militar',
-    PESO_MUERTO: 'Peso muerto',
-    DOMINADAS: 'Dominadas',
-    SENTADILLA: 'Sentadilla'
-  }
-  
-  const buildStrongRecords = strongRecords.map(strongRecord => {
-  
-  const { exercise, goal, record, ci } = strongRecord
-  
-  return {
-    title: names[exercise],
-    total: Math.round(goal * 100) / 100,
-    current: record,
-    percentaje: Math.round(ci * 100) / 100
-  }
-  
-  })
-  
-  const buildAerobicRecords = {
-    title: 'Correr',
-    total: 25,
-    current: Math.round((aerobicRecords.record.seconds / 60) * 100) / 100,
-    percentaje: Math.round(aerobicRecords.ci * 100) / 100
-  }
-  
-  const buildPowerRecord = {
-    title: 'Sprint',
-    total: 15,
-    current: powerRecords.record,
-    percentaje: Math.round(powerRecords.ci * 100) / 100
-  }
-  
-  const allRecords = [...buildStrongRecords]
-  allRecords.push(buildPowerRecord)
-  allRecords.push(buildAerobicRecords)
+	res.json({});
+});
 
+app.get("/summary-by", async (_, res) => {
+	const user = await collection.findOne({ email: "cem20903@gmail.com" });
 
-  res.json({ allRecords, totalCI })
+	const { otherGoals, englishRecords, diaryTasks } = user;
 
-})
+	const currentWeek = getWeekNumber(new Date());
+	const tasksByWeek = getTasksNumbers(otherGoals);
 
+	// Diary Task
+	const diaryTasksWithWeekNumber = diaryTasks.map((task) => {
+		return {
+			...task,
+			week: getWeekNumber(task.date),
+		};
+	});
 
-export default app
+	const diaryTasksByWeek = diaryTasksWithWeekNumber.filter(
+		(task) => task.week === currentWeek
+	);
+
+	const englishRecordsJoined = getWeekEnglishNumbers(englishRecords);
+
+	res.json({
+		diaryTasksByWeek,
+		englishRecordsByWeek: englishRecordsJoined,
+		tasksByWeek,
+	});
+});
+
+app.get("/comparative-weeks", comparativeWeeks);
+
+app.get("/comparative-percentages", async (req, res) => {
+	const user = await collection.findOne({ email: "cem20903@gmail.com" });
+	const { summary } = user;
+
+	const titles = summary[summary.length - 1].summary.map((goal) => goal.title);
+
+	res.json({ titles, summary });
+});
+
+app.get("/comparative-weeks-all-time", comparativeWeeeksAllTime);
+
+app.get("/info-ci", async (req, res) => {
+	const infoCI = await getRecords();
+
+	const { strongRecords, aerobicRecords, powerRecords, totalCI } = infoCI;
+
+	const names = {
+		PRESS_BANCA: "Press banca",
+		PRESS_MILITAR: "Press militar",
+		PESO_MUERTO: "Peso muerto",
+		DOMINADAS: "Dominadas",
+		SENTADILLA: "Sentadilla",
+	};
+
+	const buildStrongRecords = strongRecords.map((strongRecord) => {
+		const { exercise, goal, record, ci } = strongRecord;
+
+		return {
+			title: names[exercise],
+			total: Math.round(goal * 100) / 100,
+			current: record,
+			percentaje: Math.round(ci * 100) / 100,
+		};
+	});
+
+	const buildAerobicRecords = {
+		title: "Correr",
+		total: 25,
+		current: Math.round((aerobicRecords.record.seconds / 60) * 100) / 100,
+		percentaje: Math.round(aerobicRecords.ci * 100) / 100,
+	};
+
+	const buildPowerRecord = {
+		title: "Sprint",
+		total: 15,
+		current: powerRecords.record,
+		percentaje: Math.round(powerRecords.ci * 100) / 100,
+	};
+
+	const allRecords = [...buildStrongRecords];
+	allRecords.push(buildPowerRecord);
+	allRecords.push(buildAerobicRecords);
+
+	res.json({
+		allRecords,
+		totalCI,
+	});
+});
+
+// const mappedNames = {
+// 	"Coeficiente Iron (beta)": "CI",
+// 	"Peso (beta)": "WEIGHT",
+// 	Peso: "WEIGHT",
+// 	"Economia (beta) ": "ECONOMY",
+// 	"Carrera Laboral": "WORK",
+// 	First: "FIRST",
+// 	"Cosas de Casa": "HOUSE",
+// 	Lectura: "READ",
+// 	"Instagram Alfara": "INSTAGRAM_ALFARA",
+// 	"App Goal Tracker": "GOAL_TRACKER",
+// 	"Coeficiente Iron": "CI",
+// 	"Marca Personal": "PERSONAL_BRAND",
+// 	"App Coeficiente Iron": "APP_CI",
+// 	Deporte: "SPORTS",
+// 	"Habilidades Sociales": "HS",
+// 	Ingles: "ENGLISH",
+// };
+
+export default app;
